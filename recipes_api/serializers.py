2531cf3c42253eb764839f8e11b068471ecd1302
ayk-dev <70164850+ayk-dev@users.noreply.github.com>
2021-08-09 02:24:58 -0500

    Update serializers.py
    Added validate method

--- recipes_api/serializers.py
+++ recipes_api/serializers.py
@@ -4,6 +4,17 @@ from recipes_api.models import RecipeModel
 
 
 class RecipeSerializer(serializers.ModelSerializer):
+   
+    def validate(self, data):
+        self.validate_title(data["title"])
+        return data
+
+    def validate_title(self, title):
+        if title:
+            if not title[0].isupper():
+                raise serializers.ValidationError("Title must start with capital")
+        return title
+
     class Meta:
         model = RecipeModel
         fiels = '__all__'
